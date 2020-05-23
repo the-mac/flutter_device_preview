17b339407ee93205fda76530a49d75816015341e
Christopher Miller <christopher.d.miller.1@gmail.com>
2020-05-22 22:09:53 -0400

    Updating close button function on pop over lists

--- device_preview/lib/src/device_preview_lib.dart
+++ device_preview/lib/src/device_preview_lib.dart
@@ -622,7 +622,7 @@ class DevicePreviewState extends State<DevicePreview> {
   DevicePreviewStyle _style;
 
   /// The current state for closing devices popover.
-  bool _closeOnSelection;// = true;
+  bool _closeOnSelection = true;
 
   /// The default locale from the device.
   String get _defaultLocale => basicLocaleListResolution(
